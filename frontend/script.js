c1fe2adc64c165eb94ccf68d10e278fb40d868ab
SandraE-bit <sandra.ernas@outlook.com>
2025-09-04 22:03:09 +0200

    script

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,15 @@
 document.getElementById("send").addEventListener("click", function()
 {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+
+    var data = { name: fname + " " + lname }
+
     fetch("https://registration-function-g3hpc7fybuggb0ev.swedencentral-01.azurewebsites.net/api/RegisterVisitor",
     {
         method: "POST",
         headers: {"content-Type": "application/json"},
-        body: JSON.stringify( {name: name} )
+        body: JSON.stringify(data)
     })
 
     .then(function(res) { return res.text(); })
