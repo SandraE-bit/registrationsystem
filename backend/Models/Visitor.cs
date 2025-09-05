807dacf39bf781d4a8b870fb202114197489c97a
SandraE-bit <sandra.ernas@outlook.com>
2025-09-05 21:25:11 +0200

    newtonsoftjson

--- backend/Models/Visitor.cs
+++ backend/Models/Visitor.cs
@@ -1,11 +1,18 @@
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace backend.Models;
 
-public class Visitor
+public partial class Article
 {
-    [JsonPropertyName("id")]
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    [JsonProperty("id")]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [JsonProperty("createdBy")]
+    public Guid CreatedBy { get; set; }
+
+    [JsonProperty("createdOn")]
+    public DateTime CreatedOn { get; set; }
+
+    [JsonProperty("body")]
+    public string Body { get; set; } = "";
 }
