0725193dff39dba295752ebdfb87a4ce283fb2ee
SandraE-bit <sandra.ernas@outlook.com>
2025-09-05 19:34:10 +0200

    script

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,36 @@
-async function registerVisitor() {
-    const name = document.getElementById("nameInput").value;
+const button = document.getElementById("registerBtn");
+
+button.addEventListener("click", async function() {
+
+    const firstName = document.getElementById("fname").value;
+    const lastName = document.getElementById("lname").value;
+
+    const fullName = firstName + " " + lastName;
+
+    if (fullName.trim() === "") {
+        alert("First name and lastname");
+        return; 
+    }
 
     try {
-        const response = await fetch("https://registration-function-g3hpc7fybuggb0ev.swedencentral-01.azurewebsites.net/api/RegisterVisitor", {
+         const response = await fetch("https://registration-function-g3hpc7fybuggb0ev.swedencentral-01.azurewebsites.net/api/RegisterVisitor", {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ name })
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: fullName })
         });
-
+        
         const result = await response.text();
-        alert(result);
-    } catch (err) {
-        console.error("Fel vid registrering:", err);
-        alert("Det gick inte att registrera besökare.");
-    }
-}
+       
+        document.getElementById("result").innerText = result;
 
-document.getElementById("registerBtn").addEventListener("click", registerVisitor);
+    } catch (error) {
+        console.error("error", error);
+        alert("error visitor");
+    }
+});
+
 
 
 
