7d9fc3505fc9810fe5efa8150d2b52718ba6cc02
SandraE-bit <sandra.ernas@outlook.com>
2025-09-05 21:06:42 +0200

    id fix

--- backend/Models/Visitor.cs
+++ backend/Models/Visitor.cs
@@ -7,5 +7,5 @@ public class Visitor
     [JsonPropertyName("id")]
     public string Id { get; set; }
     public string Name { get; set; }
-    public DateTime Timestamp { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
 }
