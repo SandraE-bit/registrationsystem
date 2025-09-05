c2149e92c083c81e0fab9b3a065c9d1a277510ec
SandraE-bit <sandra.ernas@outlook.com>
2025-09-05 08:26:03 +0200

    added cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -5,6 +5,14 @@ using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://sandrae-bit.github.io").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 builder.ConfigureFunctionsWebApplication();
 
 builder
