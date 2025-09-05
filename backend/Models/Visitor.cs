b59cdf2ad9d40c7e3ea641b67e6725b2f0b98ff2
SandraE-bit <sandra.ernas@outlook.com>
2025-09-05 20:43:29 +0200

    id fix

--- backend/Models/Visitor.cs
+++ backend/Models/Visitor.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace backend.Models;
 
 public class Visitor
 {
+    [JsonPropertyName("id")]
     public string? Id { get; set; }
     public string? Name { get; set; }
     public DateTime Timestamp { get; set; }
