69c04ac00695672cf436ee18e2e39ff7d8cb42c3
SandraE-bit <sandra.ernas@outlook.com>
2025-09-06 21:16:05 +0200

    js name

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ button.addEventListener("click", async function () {
         return;
     }
 
-    const data = { Name: fullName };
+    const data = { name: fullName };
 
     try {
         const response = await fetch("https://registration-function-g3hpc7fybuggb0ev.swedencentral-01.azurewebsites.net/api/RegisterVisitor", {
