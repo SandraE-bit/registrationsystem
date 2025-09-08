472d7a5ac3f2647090303f605c669725cc056c48
SandraE-bit <sandra.ernas@outlook.com>
2025-09-08 11:33:45 +0200

    fix

--- backend/Models/Visitor.cs
+++ backend/Models/Visitor.cs
@@ -1,15 +1,1 @@
-using Newtonsoft.Json;
 
-namespace backend.Models;
-
-public class Visitor
-{
-    [JsonProperty("id")]
-    public string Id { get; set; } = default!;
-
-    [JsonProperty("name")]
-    public string? Name { get; set; }
-
-    [JsonProperty("timestamp")]
-    public DateTime Timestamp { get; set; }
-}
